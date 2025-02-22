16068c7129616d76769f37f1308f83f8f298fd69
Bernardo <bernardoleivasa@gmail.com>
2025-02-22 11:47:43 +0100

    init_file_fix_process_import_name

--- src/sales_data/__init__.py
+++ src/sales_data/__init__.py
@@ -1,5 +1,5 @@
 from .processing import (
-    process_output_one,
+    process_it_data,
 )
 from .utils import read_csv, write_csv
 from .models import EmployeeExpertiseAndCallsInfo, EmployePersonalAndSalesInfo
@@ -7,7 +7,7 @@ from .config import spark_session
 
 
 __all__ = [
-    "process_output_one",
+    "process_it_data",
     "read_csv",
     "write_csv",
     "EmployeeExpertiseAndCallsInfo",
