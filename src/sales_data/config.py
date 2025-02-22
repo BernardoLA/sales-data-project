6a14e5a5a8d3e174ca2482ef099e754e7f3ee901
Bernardo <bernardoleivasa@gmail.com>
2025-02-22 19:46:16 +0100

    read_validation_and_marketing_output

--- src/sales_data/config.py
+++ src/sales_data/config.py
@@ -1,15 +1,33 @@
 from pyspark.sql import SparkSession
 from pathlib import Path
-
-# Spark Session
-spark_session = (
-    SparkSession.builder.master("local[*]")
-    .appName("ABN AMRO Programming Exercise")
-    .getOrCreate()
-)
+import logging
+from logging.handlers import RotatingFileHandler
 
 # global path variables
 BASE_DIR = Path(__file__).resolve().parent
 DATA_DIR = BASE_DIR / "../../data"
 INPUT_FILE = DATA_DIR / "input/"
 OUTPUT_FILE = DATA_DIR / "output/"
+
+
+# Logging settings
+LOG_DIR = Path(__file__).resolve().parent / "../logs"
+LOG_DIR.mkdir(exist_ok=True)
+
+LOG_FILE = LOG_DIR / "app.log"
+
+# Configure Rotating Logs
+logging.basicConfig(
+    handlers=[RotatingFileHandler(LOG_FILE, maxBytes=1000000, backupCount=5)],
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+logger = logging.getLogger(__name__)
+
+# Spark Session
+spark = (
+    SparkSession.builder.master("local[*]")
+    .appName("ABN AMRO Programming Exercise")
+    .getOrCreate()
+)
---
+++ src/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="sales_data", packages=find_packages())
